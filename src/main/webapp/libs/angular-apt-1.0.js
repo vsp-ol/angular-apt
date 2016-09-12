4c6b6441573391c5f99a764d5e1845549a1dd34d
Jonas Prellberg <jonas.prellberg@uni-oldenburg.de>
2016-09-12 15:30:30 +0200

    Updated angular js module to use new ModuleRunner endpoint.

--- src/main/webapp/libs/angular-apt-1.0.js
+++ src/main/webapp/libs/angular-apt-1.0.js
@@ -1,20 +1,32 @@
 angular.module("apt", [])
 .value("serverEndpoint", "http://angular-apt.azurewebsites.net/api/")
 .service("apt", ["$http", "serverEndpoint", function apt($http, serverEndpoint) {
-    this.getCoverabilityGraph = function(pn) {
-        return $http.post(serverEndpoint + "coverabilityGraph", {pn: pn})
+	this.getCoverabilityGraph = function(pn) {
+        return $http.post(serverEndpoint + "moduleRunner", {
+    		moduleName: "coverability_graph",
+    		moduleParams: { pn: pn }
+    	})
     }
 
     this.getSynthesizedNet = function(lts, options) {
-        return $http.post(serverEndpoint + "synthesize", {lts: lts, options: options})
+        return $http.post(serverEndpoint + "moduleRunner", {
+    		moduleName: "synthesize",
+    		moduleParams: { lts: lts, options: options }
+    	})
     }
 
     this.examinePn = function(pn) {
-        return $http.post(serverEndpoint + "examinePn", {pn: pn})
+    	return $http.post(serverEndpoint + "moduleRunner", {
+    		moduleName: "examine_pn",
+    		moduleParams: { pn: pn }
+    	})
     }
 
     this.examineLts = function(lts) {
-        return $http.post(serverEndpoint + "examineLts", {lts: lts})
+        return $http.post(serverEndpoint + "moduleRunner", {
+    		moduleName: "examine_lts",
+    		moduleParams: { lts: lts }
+    	})
     }
 
     this.normalizeApt = function(apt) {
